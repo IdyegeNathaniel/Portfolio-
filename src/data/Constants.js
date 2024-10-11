927dcb5d1b44bb5f0cf949a797a810ba1a24a8f8
Idyege Nathaniel <idyegenathaniel@gmail.com>
2024-10-11 02:00:42 +0100

    Commit this

--- src/data/Constants.js
+++ src/data/Constants.js
@@ -2,8 +2,8 @@ export const Info =
   {
     "name": "Nathaniel Idyege",
     "experience":
-      "Hey there, I'm a frontend web developer passionate about transforming ideas into responsive as well as visually appealing web applications. With a solid foundation in HTML, CSS, JavaScript, and modern tools like ReactJS, Tailwind CSS, and Styled Components, I've had hands-on experience integrating REST APIs.",
+      "Hey there, I'm a frontend web developer passionate about transforming ideas into responsive, as well as visually appealing web applications. With a solid foundation in HTML, CSS, JavaScript, and modern tools like ReactJS, Tailwind CSS, and Styled Components, I've had hands-on experience integrating REST APIs.",
     "experiencePara":
-      "I focus on creating engaging and user-friendly experiences and would enjoy collaborating with a dynamic team to drive innovative solutions. I'm always looking for opportunities to grow as a developer, stay updated with industry trends, and deliver high-quality work that enhances user experience.",
+      "I focus on creating engaging and user-friendly experiences and i would enjoy collaborating with a dynamic team, to drive innovative solutions. I'm always looking for opportunities to grow as a developer, stay updated with industry trends, and deliver high-quality work that enhances user experience.",
   }
 

8648658442fc613979f4d5d9ca43237c56ed35c1
Idyege Nathaniel <idyegenathaniel@gmail.com>
2024-10-30 23:47:05 +0100

    Resume Update

--- src/data/Constants.js
+++ src/data/Constants.js
@@ -1,9 +1,12 @@
 export const Info = 
   {
-    "name": "Nathaniel Idyege",
-    "experience":
+    name: "Nathaniel Idyege",
+    role: 'I am a Frontend Developer.',
+    aim: "I'm currently focused on creating visually appealing, and user-friendly web applications that provide excellent user experience.",
+    experience:
       "Hey there, I'm a frontend web developer passionate about transforming ideas into responsive, as well as visually appealing web applications. With a solid foundation in HTML, CSS, JavaScript, and modern tools like ReactJS, Tailwind CSS, and Styled Components, I've had hands-on experience integrating REST APIs.",
-    "experiencePara":
+    experiencePara:
       "I focus on creating engaging and user-friendly experiences and i would enjoy collaborating with a dynamic team, to drive innovative solutions. I'm always looking for opportunities to grow as a developer, stay updated with industry trends, and deliver high-quality work that enhances user experience.",
+    resume: "https://drive.google.com/file/d/1bdHB7k5KvR_VC8KCnfdlDaVmE78f8a-j/view?usp=drivesdk",
   }
 
